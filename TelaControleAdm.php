ee3b825ff2a81d4d14e12c3a4b95fe2c298a02bb
bruno201405 <brunoaugustomb99@gmail.com>
2025-04-13 01:26:40 -0300

    Add files via upload

--- TelaControleAdm.php
+++ TelaControleAdm.php
@@ -1,3 +1,82 @@
+<?php
+require_once("perpage.php");	
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+
+// Inicialização das variáveis
+$id = "";
+$modelo = "";
+$marca = "";
+$ano = "";
+$placa = "";
+$precodiaria = "";
+$disponivel = "";
+
+// Montagem da consulta de pesquisa
+$queryCondition = "";
+if (isset($_POST["search"]) && is_array($_POST["search"])) {
+    foreach ($_POST["search"] as $k => $v) {
+        if (!empty($v)) {
+            $queryCases = array("modelo", "marca", "ano", "placa", "precodiaria", "disponivel");
+            if (in_array($k, $queryCases)) {
+                if (!empty($queryCondition)) {
+                    $queryCondition .= " AND ";
+                } else {
+                    $queryCondition .= " WHERE ";
+                }
+            }
+            switch ($k) {
+                case "modelo":
+                    $modelo = $v;
+                    $queryCondition .= "Modelo LIKE '%" . $v . "%'";
+                    break;
+                case "marca":
+                    $marca = $v;
+                    $queryCondition .= "Marca LIKE '%" . $v . "%'";
+                    break;
+                case "ano":
+                    $ano = $v;
+                    $queryCondition .= "Ano LIKE '%" . $v . "%'";
+                    break;
+                case "placa":
+                    $placa = $v;
+                    $queryCondition .= "Placa LIKE '%" . $v . "%'";
+                    break;
+                case "precodiaria":
+                    $precoDiaria = $v;
+                    $queryCondition .= "PrecoDiaria LIKE '%" . $v . "%'";
+                    break;
+                case "disponivel":
+                    $disponivel = $v;
+                    $queryCondition .= "Disponivel LIKE '%" . $v . "%'";
+                    break;
+            }
+        }
+    }
+}
+
+$orderby = "ORDER BY id desc"; 
+$sql = "SELECT * FROM carros " . $queryCondition;
+$href = 'TelaControleAdm';					
+
+$perPage = 10; 
+$page = 1;
+if (isset($_POST['page'])) {
+    $page = $_POST['page'];
+}
+$start = ($page - 1) * $perPage;
+if ($start < 0) $start = 0;
+$query = $sql . $orderby . " limit " . $start . "," . $perPage; 
+$result = $db_handle->runQuery($query);
+
+$conexao = new PDO("mysql:host=localhost;dbname=locacaocarros;charset=utf8", "root", "");
+$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if (!empty($result)) {
+    $result["perpage"] = showperpage($href, $sql, $perPage);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="br">
 <head>
@@ -9,160 +88,76 @@
 
 <body>
 <div class="form-content" id="form">
-    <?php
-	require_once("perpage.php");	
-	require_once("dbcontroller.php");
-	$db_handle = new DBController();
-	
-	$id = "";
-	$Modelo = "";
-	$Marca = "";
-    $Ano = "";
-	$Placa = "";
-	$PrecoDiaria = "";
-	$Disponivel = "";
-	
-	$queryCondition = "";
-	if(!empty($_POST["search"])) {
-		foreach($_POST["search"] as $k=>$v){
-			if(!empty($v)) {
-
-				$queryCases = array("Modelo","Marca","Ano", "Placa","PrecoDiaria","Disponivel");
-				if(in_array($k,$queryCases)) {
-					if(!empty($queryCondition)) {
-						$queryCondition .= " AND ";
-					} else {
-						$queryCondition .= " WHERE ";
-					}
-				}
-				switch($k) {
-					case "Modelo":
-						$Modelo = $v;
-						$queryCondition .= "Modelo LIKE '%" . $v . "%'";
-						break;
-					case "Marca":
-						$Marca = $v;
-						$queryCondition .= "Marca LIKE '%" . $v . "%'";
-						break;	
-                    case "Ano":
-                        $Ano = $v;
-                        $queryCondition .= "Ano LIKE '%" . $v . "%'";
-                        break;
-					case "Placa":
-						$Placa = $v;
-						$queryCondition .= "Placa LIKE '%" . $v . "%'";
-						break;
-					case "PrecoDiaria":
-						$PrecoDiaria = $v;
-						$queryCondition .= "PrecoDiaria LIKE '%" . $v . "%'";
-						break;
-					case "Disponivel":
-						$Disponivel = $v;
-						$queryCondition .= "Disponivel LIKE '%" . $v . "%'";
-						break;	
-
-				}
-			}
-		}
-	}
-	$orderby = "ORDER BY id desc"; 
-	$sql = "SELECT * FROM carros " . $queryCondition;
-	$href = 'TelaControleAdm';					
-		
-	$perPage = 10; 
-	$page = 1;
-	if(isset($_POST['page'])){
-		$page = $_POST['page'];
-	}
-	$start = ($page-1)*$perPage;
-	if($start < 0) $start = 0;
-	$query =  $sql . $orderby .  " limit " . $start . "," . $perPage; 
-	$result = $db_handle->runQuery($query);
-	$conexao = new PDO("mysql:host=localhost;dbname=locacaocarros;charset=utf8", "root", "");
-	$conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-
-	
-	if(!empty($result)) {
-		$result["perpage"] = showperpage($href, $sql, $perPage);
-	}
-?>
     <div id="toys-grid">      
-    <h1 id="title">Bem-vindo ao Your Guide</h1>
-	<br>
-	<p id="title1">Aqui você pode personalizar a experiência do seu consumidor fazendo o manuseio dos produtos cadastrados. Tudo o que estiver cadastrado aqui você poderá ser acessado pela Alexa via comando de voz.</p>
-	<br>
-			<form name="frmSearch" method="post" action="TelaControleAdm.php" charset="UTF-8">
-			<div class="search-box">
-			<p>
-			<input type="text" placeholder="Modelo" name="search[Modelo]" class="demoInputBox" value="<?php  echo $Modelo; ?>"	/>
-            <input type="text" placeholder="Marca" name="search[Marca]" class="demoInputBox" value="<?php echo $Marca; ?>"	/>
-            <input type="text" placeholder="Ano" name="search[Ano]" class="demoInputBox" value="<?php echo $Ano; ?>"	/>
-			<input type="text" placeholder="Placa" name="search[Placa]" class="demoInputBox" value="<?php echo $Placa; ?>"	/>
-			<input type="text" placeholder="PrecoDiaria" name="search[PrecoDiaria]" class="demoInputBox" value="<?php echo $PrecoDiaria; ?>"	/>
-			<input type="text" placeholder="Disponivel" name="search[Disponivel]" class="demoInputBox" value="<?php echo $Disponivel; ?>"	/>
-            <input type="submit" name="go" class="btnSearch" value="Buscar" id="buscar">
-            <input type="reset" class="btnSearch" value="Limpar" onclick="window.location='TelaControleAdm.php'">
-			</p>			
-			</div>
-			<br>
-			<br>
-			<table cellpadding="10" cellspacing="1">
-        <thead>
-					<tr>
-          
-		  <th><strong>Modelo</strong></th>
-          <th><strong>Marca</strong></th>          
-          <th><strong>Ano</strong></th>
-		  <th><strong>Placa</strong></th>
-		  <th><strong>PrecoDiaria</strong></th>
-		  <th><strong>Disponivel</strong></th>
-					
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					if(!empty($result)) {
-						foreach($result as $k=>$v) {
-						  if(is_numeric($k)) {
-					?>
-          <tr>
-					
-					<td class="desc"><center><?php echo $result[$k]["Modelo"]; ?></center></td>
-                    <td class="desc"><center><?php echo $result[$k]["Marca"]; ?></center></td>
-                    <td class="desc"><center><?php echo $result[$k]["Ano"]; ?></center></td>
-					<td class="desc"><center><?php echo $result[$k]["Placa"]; ?></center></td>
-					<td class="desc"><center><?php echo $result[$k]["PrecoDiaria"]; ?></center></td>
-					<td class="desc"><center><?php echo $result[$k]["Disponivel"]; ?></center></td>
-
-					<td>
-					<a class="btnEditAction" href="edit.php?id=<?php echo $result[$k]["id"]; ?>">Editar</a>
-					<a class="btnDeleteAction" href="delete.php?action=delete&id=<?php echo $result[$k]["id"]; ?>" onclick="return confirm('Você tem certeza que deseja excluir esse produto?')">Apagar</a>
-				    </td>
-
-					</tr>
-
-					<?php
-						  }
-					   }
+        <h1 id="title">Bem-vindo ao Your Guide</h1>
+        <br>
+        <p id="title1">Aqui você pode personalizar a experiência do seu consumidor fazendo o manuseio dos produtos cadastrados. Tudo o que estiver cadastrado aqui poderá ser acessado pela Alexa via comando de voz.</p>
+        <br>
+        <form name="frmSearch" method="post" action="TelaControleAdm.php" charset="UTF-8">
+            <div class="search-box">
+                <p>
+                    <input type="text" placeholder="Modelo" name="search[modelo]" class="demoInputBox" value="<?php echo htmlspecialchars($modelo); ?>" />
+                    <input type="text" placeholder="Marca" name="search[marca]" class="demoInputBox" value="<?php echo htmlspecialchars($marca); ?>" />
+                    <input type="text" placeholder="Ano" name="search[ano]" class="demoInputBox" value="<?php echo htmlspecialchars($ano); ?>" />
+                    <input type="text" placeholder="Placa" name="search[Ppaca]" class="demoInputBox" value="<?php echo htmlspecialchars($placa); ?>" />
+                    <input type="text" placeholder="PrecoDiaria" name="search[precodiaria]" class="demoInputBox" value="<?php echo htmlspecialchars($precodiaria); ?>" />
+                    <input type="text" placeholder="Disponivel" name="search[disponivel]" class="demoInputBox" value="<?php echo htmlspecialchars($disponivel); ?>" />
+                    <input type="submit" name="go" class="btnSearch" value="Buscar" id="buscar">
+                    <input type="reset" class="btnSearch" value="Limpar" onclick="window.location='TelaControleAdm.php'">
+                </p>			
+            </div>
+            <br><br>
+            <table cellpadding="10" cellspacing="1">
+                <thead>
+                    <tr>
+                        <th><strong>Modelo</strong></th>
+                        <th><strong>Marca</strong></th>          
+                        <th><strong>Ano</strong></th>
+                        <th><strong>Placa</strong></th>
+                        <th><strong>PrecoDiaria</strong></th>
+                        <th><strong>Disponivel</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                if (!empty($result)) {
+                    foreach ($result as $k => $v) {
+                        if (is_numeric($k)) {
+                ?>
+                    <tr>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["modelo"]); ?></center></td>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["marca"]); ?></center></td>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["ano"]); ?></center></td>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["placa"]); ?></center></td>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["precodiaria"]); ?></center></td>
+                        <td class="desc"><center><?php echo htmlspecialchars($v["disponivel"]); ?></center></td>
+                        <td>
+                            <a class="btnEditAction" href="edit.php?id=<?php echo $v["id"]; ?>">Editar</a>
+                            <a class="btnDeleteAction" href="delete.php?action=delete&id=<?php echo $v["id"]; ?>" onclick="return confirm('Você tem certeza que deseja excluir esse produto?')">Apagar</a>
+                        </td>
+                    </tr>
+                <?php
+                        }
                     }
-					if(isset($result["perpage"])) {
-					?>
-					<tr>
-					<td colspan="7" align="right"> <?php echo $result["perpage"]; ?></td>
-					</tr>
-					<?php } ?>
-				<tbody>
-			</table>
-			<br>
-			<table>
-			<tr>
-					<td>
-					<a id="btnAddAction" href="add.php">Adicionar um novo veículo</a> 
-					</td>
-			</tr>
-			</table>
-			</form>	
+                }
+                if (isset($result["perpage"])) {
+                ?>
+                    <tr>
+                        <td colspan="7" align="right"> <?php echo $result["perpage"]; ?></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+            <br>
+            <table>
+                <tr>
+                    <td>
+                        <a id="btnAddAction" href="add.php">Adicionar um novo veículo</a> 
+                    </td>
+                </tr>
+            </table>
+        </form>	
+    </div>
 </div>
 </body>
 </html>
--- edit.php
+++ edit.php
@@ -3,12 +3,13 @@ require_once("dbcontroller.php");
 $db_handle = new DBController();
 
 if (!empty($_POST["submit"])) {
+    // Atualizar as informações no banco de dados
     $query = "UPDATE carros SET 
                 modelo = '" . $_POST["modelo"] . "', 
                 marca = '" . $_POST["marca"] . "', 
                 ano = '" . $_POST["ano"] . "', 
                 placa = '" . $_POST["placa"] . "', 
-                categoria = '" . $_POST["categoria"] . "' 
+                precodiaria = '" . $_POST["precodiaria"] . "'
               WHERE id = " . $_GET["id"];
     
     $result = $db_handle->executeQuery($query);
@@ -37,15 +38,32 @@ $result = $db_handle->runQuery("SELECT * FROM carros WHERE id='" . $_GET["id"] .
 
     <div class="form">
         <form name="frmCarros" method="post" action="" id="frmCarros">
-            <h3 id="titulo">Edite as informações do carro</h3>
-            
-            <div>
+            <h3 id="titulo">Edite as informações do carro</h3>            
+            <div class="form-group">
                 <label class="title">Modelo</label>
-                <input type="text" name="modelo" class="demoInputBox" value="<?php echo $result[0]["modelo"]; ?>">
-            </div>
-            
-            <div>
+                <input type="text" name="modelo" class="demoInputBox" value="<?php echo $result[0]["modelo"]; ?>" required>
+            </div>            
+            <div class="form-group">
                 <label class="title">Marca</label>
-                <input type="text" name="marca" class="demoInputBox" value="<?php echo $result[0]["marca"]; ?>">
+                <input type="text" name="marca" class="demoInputBox" value="<?php echo $result[0]["marca"]; ?>" required>
+            </div>
+            <div class="form-group">
+                <label class="title">Ano</label>
+                <input type="text" name="ano" class="demoInputBox" value="<?php echo $result[0]["ano"]; ?>" required>
+            </div>
+            <div class="form-group">
+                <label class="title">Placa</label>
+                <input type="text" name="placa" class="demoInputBox" value="<?php echo $result[0]["placa"]; ?>" required>
+            </div>
+            <div class="form-group">
+                <label class="title">Preço da Diária</label>
+                <input type="text" name="precodiaria" class="demoInputBox" value="<?php echo $result[0]["precodiaria"]; ?>" required>
             </div>
             
+            <!-- Botão de envio -->
+            <div class="form-group">
+                <input type="submit" name="submit" class="btnEditAction" value="Salvar alterações">
+            </div>
+        </form>
+    </div>
+</div>
